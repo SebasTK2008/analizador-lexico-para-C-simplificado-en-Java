2a1ed35cb016959f478bfe9a8bdfab7875f88534
SebasTK2008 <sebastianlopez200802@gmail.com>
2026-09-08 23:36:31 -0500

    Add lector-archivo-c implementation

---
+++ bin/input/Main.java
@@ -0,0 +1,64 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class Main {
+        public static void main(String[] args) {
+            
+        JFileChooser selector = new JFileChooser();
+
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("C or txt archive", ".c", ".txt");
+        selector.setFileFilter(filter);
+
+        
+        int resultado = selector.showOpenDialog(null);
+
+        
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            // Obtener el archivo seleccionado
+            File selectedArchive = selector.getSelectedFile();
+            
+             Path path = Path.of(selectedArchive.getAbsolutePath());
+
+              JOptionPane.showMessageDialog(null, "Archivo seleccionado: " + path.toString());
+
+            try{
+
+                String textFile = Files.readString(path);
+                System.out.println(textFile);
+                char[] charArray = textFile.toCharArray();
+
+                System.out.println("Caracteres en el archivo:");
+                for (char c : charArray) {
+                    System.out.print(c);
+                }
+
+            } catch (IOException e) {
+                System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+            }    
+         
+            
+        } else {
+            JOptionPane.showMessageDialog(null, "Operación cancelada por el usuario.");
+        }
+    
+           
+            
+            
+
+
+
+
+
+
+
+
+            
+    
+        }
+}
